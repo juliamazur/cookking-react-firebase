245829e42436dd1a472195607245a53187e941a7
juliamazur <julasia@gmail.com>
2018-10-24 14:49:16 +0200

    schedule item styling

--- src/components/schedule/ScheduleItem.js
+++ src/components/schedule/ScheduleItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { withStyles } from '@material-ui/core/styles';
 import { Draggable } from 'react-beautiful-dnd';
 
 import Card from '@material-ui/core/Card';
@@ -13,8 +14,51 @@ const CardContainer = styled.div`
   margin: 15px 5px;
 `;
 
+const styles = theme => ({
+    avatar: {
+      cursor: 'pointer',
+    },
+});
+
 class Item extends React.Component {
+
+    state  = {
+        animation: false,
+    };
+
+    animateStart = () => {
+        this.setState({ animation: true });
+    }
+
+    animateStop = () => {
+        this.setState({ animation: false });
+    }
+
+    renderAvatar() {
+        const { classes } = this.props;
+
+        if(this.state.animation) {
+            return (
+                <Avatar className={classes.avatar}>
+                    <ClearIcon
+                        onClick={() => this.props.handleRemoveItem(this.props.item.id)}
+                    />
+                </Avatar>
+            );
+        }
+
+        return (
+            <Avatar
+                className={classes.avatar}
+                alt=''
+                src={this.props.item.recipe.imageUrl}
+            />
+        );
+    }
+
   render() {
+    const { classes } = this.props;
+
     return (
       <Draggable draggableId={this.props.item.id} index={this.props.index}>
         {(provided, snapshot) => (
@@ -25,17 +69,12 @@ class Item extends React.Component {
             isDragging={snapshot.isDragging}
           >
           <CardContainer>
-            <Card>
-              <ClearIcon
-                  onClick={() => this.props.handleRemoveItem(this.props.item.id)}
-              />
+            <Card
+                onMouseOver={this.animateStart}
+                onMouseOut={this.animateStop}
+            >
               <CardHeader
-                avatar={
-                  <Avatar
-                    alt=''
-                    src={this.props.item.recipe.imageUrl}
-                  />
-                }
+                avatar={this.renderAvatar()}
                 title={this.props.item.recipe.name}
               />
             </Card>
@@ -47,4 +86,4 @@ class Item extends React.Component {
   }
 }
 
-export default Item;
+export default withStyles(styles)(Item);
