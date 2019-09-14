37be2919c58fe0ff34cfcbf24102881adf67a2f5
juliamazur <julasia@gmail.com>
2019-09-14 07:41:52 +0200

    Updates

--- src/backend/index.js
+++
@@ -1,4 +0,0 @@
-export { default as fetchUserDoc } from './user/fetch';
-export { default as fetchRecipeList } from './recipe/fetchList';
-export { default as fetchSchedule } from './schedule/fetch';
-export { default as fetchAllSchedules } from './schedule/fetchAll';
