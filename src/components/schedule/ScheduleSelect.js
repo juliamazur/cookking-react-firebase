6abde53df12865f3d33090bdda0e90ca68e92d95
juliamazur <julasia@gmail.com>
2019-10-08 09:38:33 +0200

    select styling

--- src/components/schedule/ScheduleSelect.js
+++ src/components/schedule/ScheduleSelect.js
@@ -2,7 +2,6 @@ import React from 'react';
 import styled from "styled-components";
 
 import FormControl from '@material-ui/core/FormControl';
-import Input from '@material-ui/core/Input';
 import MenuItem from '@material-ui/core/MenuItem';
 import Select from '@material-ui/core/Select';
 import Grid from '@material-ui/core/Grid';
@@ -21,11 +20,10 @@ class ScheduleSelect extends React.Component {
         <Grid container>
           <Grid item xs={1}></Grid>
           <Grid item xs={10} md={2}>
-            <FormControl fullWidth={true}>
+            <FormControl fullWidth={true} variant="outlined">
               <Select
                 value={this.props.schedule ? this.props.schedule.id : ''}
                 onChange={this.props.handleScheduleChange}
-                input={<Input id="schedule"/>}
               >
                 {this.props.allSchedules.map(item => {
                   return (<MenuItem key={item.id} value={item.id}>{item.name}</MenuItem>)
